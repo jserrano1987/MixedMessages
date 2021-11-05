a3d919d69382d11366057c2beca823af177f940f
Justo Serrano <justoserrano87@gmail.com>
2021-11-05 19:52:42 +0100

    Primer commit del proyecto

---
+++ MixedMessages.js
@@ -0,0 +1,28 @@
+let jugador = ['Seba','Gaby','Stupa','Galán','Lebrón','Paquito']
+let jugada = ['sacó la bola por 3',
+              'sacó la bola por 4',
+              'falló estrpetisamente',
+              'mandó la bola directamente a la pared',
+              'hizo una dejada preciosa',
+              'se tropezó y se comió la red']
+let reaccionPublico = ['La gente aplaudió el esfuerzo del jugador',
+                        'La gente aplaudió entusiasmada',
+                        'El público empezó a gritar de la emoción',
+                        'Las gradas recriminaron al jugador su gesto, silbándole']
+
+let componerJugada = () =>{
+    const randJugador=jugador[Math.floor(Math.random()*jugador.length)];
+    const randJugada=jugada[Math.floor(Math.random()*jugada.length)];
+    const randReaccionPublico=reaccionPublico[Math.floor(Math.random()*reaccionPublico.length)];
+    const randFrase=`${randJugador} ${randJugada}. ${randReaccionPublico}.`
+    return randFrase;
+}
+
+let retransmiteWPT = (jugadaComentada) =>{
+    console.log('Buenos días, bienvenidos a todos a la retransmisión oficial de World Padel Tour');
+    console.log('Mi nombre es Lalo Alzueta y hoy...');
+    console.log(componerJugada());
+}
+
+
+retransmiteWPT();
---
+++ README.md
@@ -0,0 +1,8 @@
+# MIXED MESSAGES
+
+## INTRODUCTION
+Mixed Messages es mi primer proyecto web.  
+Se trata de un script en lenguaje **javascript** que genera un mensaje aleatorio para el usuario.
+
+## OBJETIVO
+El script generará una frase acerca de la jugada realizada por alguno de los principales jugadores del *World Padel Tour*.
